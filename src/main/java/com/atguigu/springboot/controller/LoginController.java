34935b5040a600e42a82ab5c298fef674c4cefd3
Administrator <ouhayo@qq.com>
2019-02-17 17:20:44 +0800

    addUser

--- src/main/java/com/atguigu/springboot/controller/LoginController.java
+++ src/main/java/com/atguigu/springboot/controller/LoginController.java
@@ -126,5 +126,26 @@ public class LoginController {
 		System.out.println(subject.getPrincipal());
 		return subject.toString();
 	}
-	
+	/**
+	 * /属于user角色
+	 * @RequiresRoles("user")
+	 *
+	 * //必须同时属于user和admin角色
+	 * @RequiresRoles({"user","admin"})
+	 *
+	 * //属于user或者admin之一;修改logical为OR 即可
+	 * @RequiresRoles(value={"user","admin"},logical=Logical.OR)
+	 * 1
+	 */
+	/**
+	 * //符合index:hello权限要求
+	 * @RequiresPermissions("index:hello")
+	 *
+	 * //必须同时复核index:hello和index:world权限要求
+	 * @RequiresPermissions({"index:hello","index:world"})
+	 *
+	 * //符合index:hello或index:world权限要求即可
+	 * @RequiresPermissions(value={"index:hello","index:world"},logical=Logical.OR)
+	 * 1
+	 */
 }
